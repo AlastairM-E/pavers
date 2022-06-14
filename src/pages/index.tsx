d3f750e419c7f9ce8200c5917a7430c0b12c3621
AlastairM-E <alastair.mottram-epson@outlook.com>
2022-06-14 10:43:25 +0100

    pavers | 2022.06.14 | EMergency fix for the app --> quick fix which is not correctly done. Prevetn double f;ash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/index.css";
+// import "../css/index.css";
 import { StaticImage } from "gatsby-plugin-image";
 import TrustpilotStars from "../images/TrustpilotStars";
 
